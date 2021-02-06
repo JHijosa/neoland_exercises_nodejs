54583c609b3f2030194e1140c4ff29be6c1e7d3d
Jose <josehijosa93@gmail.com>
2021-02-06 17:07:10 +0100

    solucion ejercicios debug

--- e5 debug/error5/api.js
+++ e5 debug/error5/api.js
@@ -1,61 +1,58 @@
-const express = require('express');
+const express = require("express");
 const fs = require("fs");
 
 const api = express();
 
-app.get('/api/users', (req, res) => {
+api.get("/api/users", (req, res) => {
     fs.readFile("db/users.json", (error, data) => {
         const users = JSON.parse(data);
         res.status(200).send({
-            success: 'true',
-            users: user
+            success: "true",
+            users: users,
         });
     });
 });
 
-
-api.get('/api/users/:id', (req, res) => {
+api.get("/api/users/:id", (req, res) => {
     fs.readFile("db/users.json", (error, data) => {
         const users = JSON.parse(data);
 
         const user = users.find(
-            user => user.id === req.params.id
+            (user) => user.id === Number.parseInt(req.params.id)
         );
 
-        if (user[0]) {
+        if (user) {
             res.status(200).send({
-                success: 'true',
-                user: user
+                success: "true",
+                user: user,
             });
         } else {
             res.status(200).send({
-                success: 'true',
-                message: 'Usuario no encontrado'
+                success: "true",
+                message: "Usuario no encontrado",
             });
         }
-
     });
 });
 
-
-api.get('/api/users/gender/:gender', (req, res) => {
+api.get("/api/users/gender/:gender", (req, res) => {
     fs.readFile("db/users.json", (error, data) => {
         const users = JSON.parse(data);
 
         const usersByGender = users.filter(
-            user => user.gender === req.params.gender
+            (user) => user.gender === req.params.gender
         );
 
         if (usersByGender) {
             res.status(200).send({
                 success: true,
-                message: 'Usuarios por genero encontrados',
-                users: usersByGender
+                message: "Usuarios por genero encontrados",
+                users: usersByGender,
             });
         } else {
             res.status(200).send({
                 success: true,
-                message: 'Usuarios por genero no encontrados'
+                message: "Usuarios por genero no encontrados",
             });
         }
     });
